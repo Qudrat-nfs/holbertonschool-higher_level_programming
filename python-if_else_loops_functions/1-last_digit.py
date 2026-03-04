f3d4e7b573de6647fa819b13fbcd9e7b1444e3b5
Qudrat-nfs <13551@holbertonstudents.com>
2026-03-04 15:02:57 +0000

    task1

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number > 0:
-    last_digit = number % -10
-else:
-    last_digit = number % 10
-
-output = f"last_digit of {number} is {last_digit}"
-if last_digit > 5:
-    print(f"{output} and is greater than 5")
+ 
+last_digit = abs(number) % 10
+if last_digit >5:
+    condition = "and is greater than 5"
 elif last_digit == 0:
-    print(f"{output} and is 0")
+    condition ="and is 0"
 else:
-    print(f"{output} and is less than 6 and not 0") 
+    condition = "and is less than 6 and not 0"
+print(f"Last_digit of {number} is {last_digit} {condition}")
