6aabcc77c72c00044a42365dcb3abf1d4ad9439c
Qudrat-nfs <13551@holbertonstudents.com>
2026-03-04 15:20:34 +0000

    task1

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,9 +5,9 @@ last_digit = abs(number) % 10
 if number  < 0:
     last_digit = -last_digit
 if last_digit >5:
-    condition = "and is greater than 5"
+    condition ="and is greater than 5"
 elif last_digit == 0:
     condition ="and is 0"
 else:
-    condition = "and is less than 6 and not 0"
+    condition ="and is less than 6 and not 0"
 print(f"Last digit of {number} is {last_digit} {condition}")
