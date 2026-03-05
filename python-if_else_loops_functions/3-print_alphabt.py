68743ab6ac88a83429e52488bd3dd814af1a0c98
Qudrat-nfs <13551@holbertonstudents.com>
2026-03-05 18:29:02 +0000

    task

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("".join("{}".format(chr(i)) for i in range(97, 123) if chr(i) != "q" and chr(i) != "e"), end="")
+print("".join("{}".format(chr(i)) for i in range(97, 123) if chr(i) != "q"
+ and chr(i) != "e"), end="")
