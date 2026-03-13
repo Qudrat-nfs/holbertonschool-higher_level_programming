e9f86f76d7377c8ef8adf21c9dea7faafb423cff
Qudrat-nfs <13551@holbertonstudents.com>
2026-03-13 18:15:38 +0000

    fs

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for number in row:
-            print("{:d}".format(number))
+        for i in range(len(row)):
+
+            if i == len(row) - 1:
+                print("{:d}".format(row[i]), end="")
+            else:
+                print("{:d}".format(row[i]), end=" ")
         print()
