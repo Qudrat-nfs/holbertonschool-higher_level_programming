a2cdfe7b497cc8e4b8ce41bd6a03c487e4463fe4
Qudrat-nfs <13551@holbertonstudents.com>
2026-03-23 16:54:27 +0000

    :h

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,4 +5,5 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
     except (ValueError, TypeError):
         pass
-        return i
+    print()
+    return i
