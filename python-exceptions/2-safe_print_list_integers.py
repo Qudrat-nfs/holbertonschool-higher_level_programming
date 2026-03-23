38f44121d571a00b143c821d6bd9eab65e9049c5
Qudrat-nfs <13551@holbertonstudents.com>
2026-03-23 17:03:00 +0000

    fsf

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        for i in range(x):
+    count = 0
+    for i in range(x):
+        try:
             print("{:d}".format(my_list[i]), end="")
-    except (ValueError, TypeError):
-        pass
-    print()
-    return i
+            count += 1
+        except (ValueError, TypeError):
+            pass
+        print()
+        return count
