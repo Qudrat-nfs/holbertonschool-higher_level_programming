f9ae4be1f49fac57bcaa0aa4fbb39e6399756b13
Qudrat-nfs <13551@holbertonstudents.com>
2026-03-27 21:04:05 +0000

    kjjhg

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -3,16 +3,16 @@
 class Square:
     """A class that defines a square."""
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize the square with a specific size and position."""
+        """Initialize the square."""
         self.size = size
         self.position = position
     @property
     def size(self):
-        """Retrieve the size of the square."""
+        """Retrieve the size."""
         return self.__size
     @size.setter
     def size(self, value):
-        """Set the size of the square with validation."""
+        """Set the size."""
         if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
@@ -20,11 +20,11 @@ class Square:
         self.__size = value
     @property
     def position(self):
-        """Retrieve the position of the square."""
+        """Retrieve the position."""
         return self.__position
     @position.setter
     def position(self, value):
-        """Set the position of the square with validation."""
+        """Set the position."""
         if (type(value) is not tuple or len(value) != 2 or
                 type(value[0]) is not int or type(value[1]) is not int or
                 value[0] < 0 or value[1] < 0):
@@ -34,11 +34,11 @@ class Square:
         """Return the area of the square."""
         return self.__size * self.__size
     def my_print(self):
-        """Print the square with the # character, using position."""
+        """Print the square with the # character."""
         if self.__size == 0:
             print()
             return
         for _ in range(self.__position[1]):
-            print()ı
+            print()
         for _ in range(self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
