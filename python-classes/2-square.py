6611da82c90635fde3cd266d07b06303f283d537
Qudrat-nfs <13551@holbertonstudents.com>
2026-03-27 19:31:32 +0000

    dfz

---
+++ python-classes/2-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""This module defines a Square class with a private size attribute."""
+class Square:
+    """A class that defines a square."""
+    def __init__(self, size = 0):
+        """Initialize the square with a specific size."""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
