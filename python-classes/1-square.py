fb222af5ed2a12c35eb51dbfafc39d410683001d
Qudrat-nfs <13551@holbertonstudents.com>
2026-03-27 19:08:23 +0000

    v

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""This module defines a Square class with a private size attribute."""
 class Square:
+    """A class that defines a square."""
     def __init__(self, size):
+        """Initialize the square with a specific size.
+        Args:
+            size: The size of the new square.
+        """
         self.__size =size
