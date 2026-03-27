322c0f844a60536277263f8e556c2a12a03405aa
Qudrat-nfs <13551@holbertonstudents.com>
2026-03-27 18:51:23 +0000

    fds

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """that is for solving  Square problems"""
 class Square:
-"""that define empty Square"""
+    """that define empty Square"""
     pass
-S = Square()
-print(str(s))
