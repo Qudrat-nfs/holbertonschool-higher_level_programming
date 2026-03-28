64faa223f8c1af6c01b07abb56147c6460c0967e
Qudrat-nfs <13551@holbertonstudents.com>
2026-03-28 18:18:09 +0000

    gfd

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
-"""Module that defines a Square class inheriting from Rectangle."""
+#!/usr/bin/python3
+"""Square module"""
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """This class defines a Square class with a private size attribute.""" 
+    """Square class"""
+
     def __init__(self, size):
-        """
-        Initialize the square with a size.
-        """
+        """Initialize"""
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
     def area(self):
-        """
-        Calculates and returns the area of the square.
-        """
+        """Return area"""
         return self.__size * self.__size
 
     def __str__(self):
-        """
-        Returns the string representation of the square.
-        Format: [Square] <width>/<height>
-        """
+        """Return string representation"""
         return "[Square] {}/{}".format(self.__size, self.__size)
