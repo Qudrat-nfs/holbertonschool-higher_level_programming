1a6fa16de5a769ad42b26d185b21e50e73ac11c9
Qudrat-nfs <13551@holbertonstudents.com>
2026-03-28 17:41:59 +0000

    vdc

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Module that defines a Rectangle class inheriting from BaseGeometry."""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     """A class representing a rectangle, inheriting from BaseGeometry."""
 
