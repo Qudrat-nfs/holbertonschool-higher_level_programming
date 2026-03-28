a5c07128f4cfb7d082dc65d26ecf8d2dc8b04c2e
Qudrat-nfs <13551@holbertonstudents.com>
2026-03-28 16:15:23 +0000

    kjh

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """a function returns the object is an instance of a
- class that inherited (directly or indirectly) from the specified class
- """
+class that inherited (directly or indirectly) from the specified class
+"""
 
 
 def inherits_from(obj, a_class):
     """a function that returns True
-if the object is an instance of a class that
+    if the object is an instance of a class that
     inherited (directly or indirectly)
-from the specified class ; otherwise False."""
+    from the specified class ; otherwise False."""
     return issubclass(type(obj), a_class) and type(obj) is not a_class
