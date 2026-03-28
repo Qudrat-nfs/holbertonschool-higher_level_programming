5d74d2e31169ecdf7d1aff1503918b5df2019483
Qudrat-nfs <13551@holbertonstudents.com>
2026-03-28 17:36:10 +0000

    jhg

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Rectangle(BaseGeometry):
+    """A class representing a rectangle, inheriting from BaseGeometry."""
+
+    def __init__(self, width, height):
+        """
+        Initialize the rectangle with width and height.
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
