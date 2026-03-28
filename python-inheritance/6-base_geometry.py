8a5c139c19fba516798b1951077f71648cc2afde
Qudrat-nfs <13551@holbertonstudents.com>
2026-03-28 16:30:06 +0000

    jh

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module that defines an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+        An empty class representing base geometry.
+    """
+    def area(self):
+        """Return the area of the geometry."""
+        raise Exception("area() is not implemented")
