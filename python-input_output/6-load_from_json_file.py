0d42b06c57fb8dd0b61ad9a1ef932b8f40adbeb4
Qudrat-nfs <13551@holbertonstudents.com>
2026-04-12 18:48:59 +0000

    fdsf

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for saving a Python object to a file in JSON format."""
+import json
+
+
+def load_from_json_file(filename):
+    """Writes an Object to a text file, using a JSON representation."""
+    with open(filename, mode="w", encoding="utf-8") as file:
+        return json.load(filname)
