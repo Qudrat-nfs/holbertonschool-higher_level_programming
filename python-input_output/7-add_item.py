ddc6eaaa5c7504980185e8deea73096503d05512
Qudrat-nfs <13551@holbertonstudents.com>
2026-04-12 19:05:22 +0000

    dsf

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""Script that adds all arguments to a list, and saves them to a file."""
+"""Script that adds arguments to a Python list, and saves to a file."""
 import sys
 
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    save_module = __import__('5-save_to_json_file')
+    save_to_json_file = save_module.save_to_json_file
+
+    load_module = __import__('6-load_from_json_file')
+    load_from_json_file = load_module.load_from_json_file
 
     filename = "add_item.json"
 
@@ -13,5 +16,6 @@ if __name__ == "__main__":
         items = load_from_json_file(filename)
     except FileNotFoundError:
         items = []
+
     items.extend(sys.argv[1:])
     save_to_json_file(items, filename)
