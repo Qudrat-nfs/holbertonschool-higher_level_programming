77a3484bde8e419159758db626e71865940c9006
Qudrat-nfs <13551@holbertonstudents.com>
2026-04-12 18:01:30 +0000

    gfd

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def write_file(filename="", text=""):
     """Reads a text file in UTF-8 format and prints it to stdout."""
     with open(filename, mode="w", encoding="utf-8") as file:
-        print(file.write(), end="")
+        return file.write(text)
