20d4a439a806d93aceffb6b63531a9e325ec3981
Qudrat-nfs <13551@holbertonstudents.com>
2026-04-12 18:37:31 +0000

    fg

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,4 +5,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """Appends a string to a text file (UTF8) and returns char count."""
     with open(filename, mode="w", encoding="utf-8") as file:
-        return json.loads(file.write(text))
+        return json.loads(file.write(my_obj))
