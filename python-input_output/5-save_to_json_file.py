1144d97501c8749c1e1962674e60212b73c4359c
Qudrat-nfs <13551@holbertonstudents.com>
2026-04-12 18:35:53 +0000

    df

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for appending a string to the end of a text file."""
+
+import json
+def save_to_json_file(my_obj, filename):
+    """Appends a string to a text file (UTF8) and returns char count."""
+    with open(filename, mode="w", encoding="utf-8") as file:
+        return json.loads(file.write(text))
