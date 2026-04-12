abf74efe7adc06da67dc45d12d19311d88478b66
Qudrat-nfs <13551@holbertonstudents.com>
2026-04-12 18:10:56 +0000

    gd

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,6 +3,6 @@
 
 
 def append_write(filename="", text=""):
-    """Writes a text file (UTF8) and returns the number of characters written."""
+    """Writes a text file (UTF8) and returns the number of charactersn."""
     with open(filename, mode="a", encoding="utf-8") as file:
         return file.write(text)
