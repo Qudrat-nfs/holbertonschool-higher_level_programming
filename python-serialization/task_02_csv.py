35559ab5e228309b198eca92f0b0fd7fb7ce8789
root <root@ip-10-42-162-64.ec2.internal>
2026-04-14 10:56:36 +0000

    jhgf

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import csv
+import json
+
+def convert_csv_to_json(csv_filename):
+    try:
+        with open(csv_filename, mode='r', encoding='utf-8') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            data_list = list(csv_reader)
+        with open('data.json', mode='w', encoding='utf-8') as json_file:
+            json.dump(data_list, json_file, indent=4)
+        return True
+    except FileNotFoundError:
+        return False
+    except Exception:
+        return False
