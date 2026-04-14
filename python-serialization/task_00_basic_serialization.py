346a1bb884657e3945aa45590f386e013685e3bc
root <root@ip-10-42-162-64.ec2.internal>
2026-04-14 10:29:25 +0000

    f

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import json
+
+def serialize_and_save_to_file(data, filename):
+    """
+    Serializes a Python dictionary and saves it to a JSON file.
+    Args:
+        data (dict): The dictionary to serialize.
+        filename (str): The name of the output JSON file.
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f)
+
+def load_and_deserialize(filename):
+    """
+    Loads a JSON file and deserializes it into a Python dictionary.
+    Args:
+        filename (str): The name of the JSON file to read.
+    Returns:
+        dict: The reconstructed Python dictionary.
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
