6c8514b9d3e1a430f471bfb5febb75313eb69945
Qudrat-nfs <13551@holbertonstudents.com>
2026-04-19 19:20:49 +0000

    hng

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
---  Updates the score of Bob to 10
-UPDATE second_table SET name = 'BOB';
+-- Updates the score of Bob to 10 in the table second_table
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
