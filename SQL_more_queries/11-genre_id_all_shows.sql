4d28985ba076dec1e92f98a7ea485e091b09f546
Qudrat-nfs <13551@holbertonstudents.com>
2026-04-23 18:42:36 +0000

    df

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
--- Lists all shows in hbtn_0d_tvshows that have at least one genre linked.
--- JOIN is used to connect shows with their corresponding genres
+-- Lists all shows contained in the database hbtn_0d_tvshows.
+-- If a show doesn't have a genre, display NULL.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
