261fe9678beadfc3bf494dffdd8fa4fdf2f0cf15
Qudrat-nfs <13551@holbertonstudents.com>
2026-04-23 18:49:31 +0000

    df

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,3 +1,4 @@
+-- mport the database dump from hbtn_0d_tvshows to your MySQL server:
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_show_id = tv_show_genres.show_id
