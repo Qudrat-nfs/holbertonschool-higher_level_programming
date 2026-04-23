24185634cde49a76e46b28df51136fa025ec98e6
Qudrat-nfs <13551@holbertonstudents.com>
2026-04-23 18:34:18 +0000

    DF

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,5 @@
--- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Lists all shows in hbtn_0d_tvshows that have at least one genre linked.
+-- Select show title and genre id, joining tv_shows and tv_show_genres
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
