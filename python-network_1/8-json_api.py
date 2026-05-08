fa6571ff507bd0fc6e70b3e19dd0f07d97feb5cb
Qudrat-nfs <13551@holbertonstudents.com>
2026-05-08 16:05:23 +0000

    fe

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -5,17 +5,13 @@ with a letter as a parameter 'q'.
 """
 import sys
 import requests
-
-
 if __name__ == "__main__":ı
     if len(sys.argv) > 1:
         q = sys.argv[1]
     else:
         q = ""
-
     url = "http://0.0.0.0:5000/search_user"
     payload = {'q': q}
-
     try:
         response = requests.post(url, data=payload)
         json_data = response.json()ı
@@ -24,6 +20,5 @@ if __name__ == "__main__":ı
         else:
             for user in json_data:
                 print("[{}] {}".format(user.get('id'), user.get('name')))
-
     except ValueError:
         print("Not a valid JSON")
