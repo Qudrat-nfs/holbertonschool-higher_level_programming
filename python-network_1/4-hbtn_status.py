63a0dbfe6d6425068e01b77aec1f88c713cb4c1d
Qudrat-nfs <13551@holbertonstudents.com>
2026-05-08 15:40:14 +0000

    d

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Fetches https://intranet.hbtn.io/status using the requests package.
+"""
+import requests
+
+
+if __name__ == "__main__":
+    response = requests.get("https://intranet.hbtn.io/status")
+    html = response.text
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
